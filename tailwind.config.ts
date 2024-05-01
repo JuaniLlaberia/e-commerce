8472fbfb9a570e9d7ce9921294386d1550525922
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-05-01 15:35:55 -0300

    Project set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,64 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'background-1': 'var(--background-1)',
+        'background-2': 'var(--background-2)',
+        'background-3': 'var(--background-3)',
+        'background-hover-1': 'var(--background-hover-1)',
+        'background-hover-2': 'var(--background-hover-2)',
+        'background-danger': 'var(--background-danger)',
+        'background-contrast': 'var(--background-contrast)',
+        'background-contrast-hover': 'var(--background-contrast-hover)',
+        'background-overlay': 'var(--background-overlay)',
+        'text-1': 'var(--text-1)',
+        'text-2': 'var(--text-2)',
+        'text-contrast': 'var(--text-contrast)',
+        'text-hover': 'var(--text-hover)',
+        'text-danger': 'var(--text-danger)',
+        'border-1': 'var(--border-1)',
+        'border-2': 'var(--border-2)',
+        special: 'var(--special)',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
+} satisfies Config;
+
 export default config;
