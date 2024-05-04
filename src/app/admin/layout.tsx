0b6002faf9f08109a4e4c60485414fad7bb26094
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-05-04 14:05:30 -0300

    Update admin route layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,11 +7,13 @@ export default async function ProjectsLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className='flex h-full'>
+    <main className='flex h-screen'>
       <Sidebar />
       <div className='flex flex-col w-full'>
         <Navbar />
-        <div className='p-5 bg-background-1 flex-1'>{children}</div>
+        <div className='p-5 lg:px-16 bg-background-1 flex-1 overflow-y-scroll overflow-x-hidden'>
+          {children}
+        </div>
       </div>
     </main>
   );
